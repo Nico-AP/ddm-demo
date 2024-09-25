6f08c25649ff8d31a8d28806ce7fa1c9f82bdb5e
Nico Pfiffner (nipfif) <n.pfiffner@ikmz.uzh.ch>
2024-09-26 01:16:57 +0200

    fix: Add missing path names.

--- ddm_demo/urls.py
+++ ddm_demo/urls.py
@@ -7,6 +7,8 @@ from allauth.account.views import SignupView
 urlpatterns = [
     path('', LandingPage.as_view(), name='ddm-demo-landing'),
     path('auth/login/', auth_views.LoginView.as_view(template_name='ddm_demo/login.html'), name='account_login'),
+    path('auth/login/', auth_views.LoginView.as_view(template_name='ddm_demo/login.html'), name='ddm-login'),
     path('auth/logout/', auth_views.LogoutView.as_view(), name='account_logout'),
+    path('auth/logout/', auth_views.LogoutView.as_view(), name='ddm-logout'),
     path('auth/register/', SignupView.as_view(), name='account_signup'),
 ]
