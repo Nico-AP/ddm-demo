ff0f44ae1bb2923a8681a8f8225a4f1385f96505
Nico-AP <n.pfiffner@ikmz.uzh.ch>
2025-02-27 09:11:38 +0100

    fix: Remove mysql specific options.

--- config/settings/production.py
+++ config/settings/production.py
@@ -72,8 +72,5 @@ DATABASES = {
         'NAME': os.environ['DJANGO_DB_NAME'],
         'USER': os.environ['DJANGO_DB_USER'],
         'PASSWORD': os.environ['DJANGO_DB_PW'],
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
     }
 }
